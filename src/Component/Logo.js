6aca585cd0743d2de8b43829b4ae65818198bdc7
VaLalent <108317198+VaLalent@users.noreply.github.com>
2023-12-20 18:53:05 +0700

    Navbar

--- src/Component/Logo.js
+++ src/Component/Logo.js
@@ -2,17 +2,15 @@ import Link from "next/link";
 import React from "react";
 import {motion} from "framer-motion";
 
-const motionLink = motion(Link);
+const MotionLink = motion(Link);
 
 const Logo = () => {
     return (
-        <div className="flex item-center justify-center mt-2">
-            <motionLink href="/"
-            className="w-16 h-16 bg-dark text-light flex item center justify-center rounded-full text-2x1 font-bold"
-            whileHover={{
-                scale: 1.5,
-            }}
-            >Nawa</motionLink>
+        <div className="flex items-center justify-center mt-2">
+            <MotionLink href="/"
+            className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2x1 font-bold"
+            whileHover={{scale: 1.5}}
+            >Nawa</MotionLink>
         </div>
     )
 }
